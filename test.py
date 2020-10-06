02241ebfb8693f6f8f38475ee6427a5de372f26e
jingyang97 <yangjing9597@gmail.com>
2020-10-05 19:24:11 -0700

    a

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import base64
 
-f = open('azure.png', 'rb')  #二进制方式打开图文件
-ls_f = base64.b64encode(f.read())  #读取文件内容，转换为base64编码
+f = open('azure.png', 'rb')
+ls_f = base64.b64encode(f.read())
 f.close()
 print(ls_f)
